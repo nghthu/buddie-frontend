c04383c0a38df6f22041613e8a02373b0c2da913
Huy201201 <20120102@student.hcmus.edu.vn>
2024-04-14 20:12:00 +0700

    added API_BASE_URL to .env, replace url with the env variable for fetch

--- src/app/api/tests/[testId]/route.ts
+++ src/app/api/tests/[testId]/route.ts
@@ -4,12 +4,11 @@ export async function GET(request: Request, { params }: { params: { testId: stri
     const testId=params.testId;
     try {
         const response = await fetch(
-          `https://q40nfgqaj4.execute-api.ap-southeast-2.amazonaws.com/api/v1/tests/${testId}`,
+          `${process.env.API_BASE_URL}/api/v1/tests/${testId}`,
           {
             method: 'GET'
           }
         );
-    
         const data = await response.json();
         return NextResponse.json(data);
       } catch (error) {
