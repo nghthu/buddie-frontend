49398794338ec61fd42b0d85626146aff78a0471
Huy201201 <20120102@student.hcmus.edu.vn>
2024-05-15 23:44:15 +0700

    added input comment for user

--- src/app/(user)/tests/[id]/page.tsx
+++ src/app/(user)/tests/[id]/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import SkillHeader from "@/components/SkillHeader";
-import { Spin, Button, notification } from 'antd';
+import { Spin, Button, notification, Empty, Input } from 'antd';
 import styles from '@/styles/pages/TestLanding.module.scss';
 import { User } from 'firebase/auth';
 import { auth } from '@/lib';
@@ -10,6 +10,7 @@ import TextCard from "@/components/TextCard";
 import TestDetails from "@/components/TestDetails";
 import PartSelector from "@/components/PartSelector";
 import Comment from "@/components/Comment";
+import { SendOutlined } from "@ant-design/icons";
 interface test_answer {
     test_id: string,
     parts: {
@@ -46,6 +47,8 @@ const fetcher = async ({ url, user }: FetchArgs) => {
     return response.data;
 };
 
+const { TextArea } = Input;
+
 export default function TestLanding({ params, }: { params: { id: string }; }) {
     const [totalPage, setTotalPage] = useState(1);
     const [totalComments, setComments] = useState([] as any[]);
@@ -58,8 +61,8 @@ export default function TestLanding({ params, }: { params: { id: string }; }) {
     } = useSWR({ url: `/api/tests/${params.id}`, user }, fetcher);
     const {
         data: comments,
-        error:errorComment,
-        isLoading:isLoadingComment,
+        error: errorComment,
+        isLoading: isLoadingComment,
     } = useSWR({ url: `/api/comment/${params.id}?page=${totalPage}`, user }, fetcher);
     const [notificationApi, contextHolder] = notification.useNotification();
     useEffect(() => {
@@ -101,17 +104,31 @@ export default function TestLanding({ params, }: { params: { id: string }; }) {
     if (isLoading) {
         return <Spin size="large" />;
     }
-    const commentSection = totalComments.map((comment) => <Comment key={comment._id} id={comment._id} userName={'lorem Ipsum'} userPhotoURL="" createdDate={comment.created_at} content={comment.comment} />);
+    const commentSection = totalComments.map((comment) => <Comment key={comment._id} id={comment._id} userName={'lorem Ipsum'} userPhotoURL={'https://fastly.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY'} createdDate={comment.created_at} content={comment.comment} />);
+    commentSection.push(<Comment key={'acssasdsad'} id={'as'} userName={'lorem Ipsum'} userPhotoURL='https://fastly.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY' createdDate={'1-1-1970'} content={'lmaasd ksadj alksdj lkasjd lsaj dlksajd lkasjd alksjd oiasj daslid jlsak dhsalkd jalsdj alksdjo lmaasd ksadj alksdj lkasjd lsaj dlksajd lkasjd alksjd oiasj daslid jlsak dhsalkd jalsdj alksdjo lmaasd ksadj alksdj lkasjd lsaj dlksajd lkasjd alksjd oiasj daslid jlsak dhsalkd jalsdj alksdjo lmaasd ksadj alksdj lkasjd lsaj dlksajd lkasjd alksjd oiasj daslid jlsak dhsalkd jalsdj alksdjo lmaasd ksadj alksdj lkasjd lsaj dlksajd lkasjd alksjd oiasj daslid jlsak dhsalkd jalsdj alksdjolmaasd ksadj alksdj lkasjd lsaj dlksajd lkasjd alksjd oiasj daslid jlsak dhsalkd jalsdj alksdjolmaasd ksadj alksdj lkasjd lsaj dlksajd lkasjd alksjd oiasj daslid jlsak dhsalkd jalsdj alksdjolmaasd ksadj alksdj lkasjd lsaj dlksajd lkasjd alksjd oiasj daslid jlsak dhsalkd jalsdj alksdjo'} />)
+    commentSection.push(<Comment key={'acssasdsad'} id={'as'} userName={'lorem Ipsum'} userPhotoURL='https://fastly.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY' createdDate={'1-1-1970'} content={'lmaasd ksadj alksdj lkasjd lsaj dlksajd lkasjd alksjd oiasj daslid jlsak dhsalkd jalsdj alksdjo lmaasd ksadj alksdj lkasjd lsaj dlksajd lkasjd alksjd oiasj daslid jlsak dhsalkd jalsdj alksdjo lmaasd ksadj alksdj lkasjd lsaj dlksajd lkasjd alksjd oiasj daslid jlsak'} />)
     const parts = test?.parts.map((part: { part_duration: number }, index: number) => { return { index: index + 1, time: part.part_duration } }) || [];
     return (
         <div className={styles.pageWrapper}>
             {contextHolder}
             <SkillHeader title={test.test_name} />
             <TextCard width={'90%'} height={'auto'} >
+                
                 <TestDetails user_name={test?.user?.display_name || "lorem ipsum"} rating={test?.review.star || 3.2} rating_count={test?.review.count || 13} update_date={test.updated_at || "2024-04-23T06:57:19.523Z"} submission_count={test.submission_count || 100} />
                 <PartSelector parts={parts} testId={params.id} />
                 <div className={styles.commentWrapper} ref={scrollRef}>
-                    {isLoadingComment ? <Spin size="large" /> : commentSection}
+                    {isLoadingComment && <Spin size="large" />}
+                    {!isLoadingComment && commentSection.length === 0 ? <Empty /> : null}
+                    {!isLoadingComment && commentSection.length > 0 ? commentSection : null}
+                </div>
+                <div className={styles.commentInputWrapper}>
+                    <div className={styles.commentInput}>
+                        <img src={'https://fastly.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY'} className={styles.avatar} alt="avatar" />
+                        <div className={styles.inputAndSendBtn}>
+                        <TextArea placeholder="Viết comment" className={styles.inputComment} autoSize/>
+                        <Button type="primary" shape="circle" icon={<SendOutlined />} size="large" />
+                        </div>
+                    </div>
                 </div>
             </TextCard>
         </div>
--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -3,9 +3,7 @@ import styles from '@/styles/components/Comment.module.scss';
 export default function Comment({ id, userName, userPhotoURL, createdDate, content }: { id: string, userName: string, userPhotoURL: string, createdDate: string, content: string }) {
     return (
         <div className={styles.wrapper}>
-            <div className={styles.avatar}>
-                <img src={userPhotoURL} style={{objectFit:'cover'}} alt="avatar" />
-            </div>
+            <img src={userPhotoURL} className={styles.avatar} alt="avatar" />
             <div className={styles.contentWrapper}>
                 <div className={styles.upper}>
                     <div className={styles.userName}>{userName}</div>
