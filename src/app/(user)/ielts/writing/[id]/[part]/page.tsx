313dd105c989f47f41430e828ddea22632fecc8a
thanhtrietla <20120222@student.hcmus.edu.vn>
2024-05-25 18:08:18 +0700

    Perform Writing Tab tasks

--- src/app/(user)/ielts/writing/[id]/[part]/page.tsx
+++ src/app/(user)/ielts/writing/[id]/[part]/page.tsx
@@ -1,14 +1,14 @@
 'use client';
 
-import { useState, useRef, useEffect, use } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import CountdownClock from '@/components/CountdownClock';
 import styles from '@/styles/pages/writing/Writing.module.scss';
 import WritingFunctionMenu from '@/components/WritingFunctionMenu';
 import Link from 'next/link';
-import { useSearchParams } from 'next/navigation';
 import BuddieSupport from '@/components/BuddieSupport';
 import { CloseChatContext } from '@/components/CloseChatContext';
 import { auth } from '@/lib';
+import { Spin } from 'antd';
 import { set } from 'lodash';
 
 type PartData =
@@ -22,13 +22,16 @@ type PartData =
     }
   | undefined;
 
-export default function PracticePage() {
+export default function PracticePage({
+  params,
+}: {
+  params: { id: string; part: string };
+}) {
   const [menuVisible, setMenuVisible] = useState(false);
   const [menuPosition, setMenuPosition] = useState({ x: 0, y: 0 });
   const textareaRef = useRef<HTMLTextAreaElement>(null);
   const [chatVisible, setChatVisible] = useState(false);
-  const searchParams = useSearchParams();
-  const part = searchParams.get('part');
+  const part = params.part;
   const [selection, setSelection] = useState<string>('');
   const [chatRequests, setChatRequests] = useState<
     Array<{ avatar: string; request: string; response: string }>
@@ -38,77 +41,46 @@ export default function PracticePage() {
     Array<{ topic: string; content: string }>
   >([]);
   const [isChatProcessing, setIsChatProcessing] = useState(false);
+  const [isDataLoading, setIsDataLoading] = useState(false);
   const user = auth.currentUser;
 
-  //Dummy data
-  const partsData = {
-    parts: [
-      {
-        part_number: 1,
-        part_duration: 1200,
-        part_recording: '',
-        part_prompt: '',
-        part_image_urls: [],
-        question_groups: [
-          {
-            is_single_question: true,
-            question_groups_info: {},
-            questions: [
-              {
-                question_number: 1,
-                question_type: 'writing',
-                question_prompt:
-                  'You should spend about 20 minutes on this task.\n\nThe bar chart below describes some changes about the percentage of people were born in Australia and who were born outside Australia living in urban, rural and town between 1995 and 2010.\nSummarise the information by selecting and reporting the main features and make comparisons where relevant.\nYou should write at least 150 words.',
-                question_image_urls: [
-                  'https://iotcdn.oss-ap-southeast-1.aliyuncs.com/2021-12/Platypus-01_0.png',
-                ],
-                question_duration: 1200,
-              },
-            ],
-          },
-        ],
+  const getPartData = async () => {
+    const token = await user?.getIdToken();
+
+    const response = await fetch(`/api/tests/${params.id}`, {
+      headers: {
+        authorization: `Bearer ${token}`,
       },
-      {
-        part_number: 2,
-        part_duration: 2400,
-        part_recording: '',
-        part_prompt: '',
-        part_image_urls: [],
-        question_groups: [
-          {
-            is_single_question: true,
-            question_groups_info: {},
-            questions: [
-              {
-                question_number: 2,
-                question_type: 'writing',
-                question_prompt:
-                  'You should spend about 40 minutes on this task.\nRich countries often give money to poorer countries, but it does not solve poverty. Therefore, developed countries should give other types of help to the poor countries rather than financial aid. To what extent do you agree or disagree?\nYou should write at least 250 words.',
-                question_image_urls: [],
-                question_duration: 2400,
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  };
+    });
+
+    const result = await response.json();
+    return result.data;
+  }
 
   const question =
     partData?.question_groups[0].questions[
       Math.floor(Math.random() * partData.question_groups[0].questions.length)
     ];
 
-  useEffect(() => {
-    let partData;
-    if (part === '1' || part === 'all') {
-      partData = partsData.parts.find((p) => p.part_number === 1);
-    } else if (part === '2') {
-      partData = partsData.parts.find((p) => p.part_number === 2);
-    }
+    useEffect(() => {
+      const fetchPartData = async () => {
+        setIsDataLoading(true);
+        const data = await getPartData();
+        let partData;
+        if (data && data.parts) {
+          if (part === '1' || part === 'all') {
+            partData = data.parts.find((p: { part_number: number; }) => p.part_number === 1);
+          } else if (part === '2') {
+            partData = data.parts.find((p: { part_number: number; }) => p.part_number === 2);
+          }
+        }
 
-    setPartData(partData);
-  }, [part]);
+        setPartData(partData);
+        setIsDataLoading(false);
+      };
+
+      fetchPartData();
+    }, [part]);
 
   useEffect(() => {
     localStorage.setItem('resultData', JSON.stringify(resultData));
@@ -200,7 +172,6 @@ export default function PracticePage() {
     });
 
     const data = await response.json();
-    console.log(data);
     return data;
   };
 
@@ -262,25 +233,55 @@ export default function PracticePage() {
           IETLS Writing Task {partData?.part_number}
         </h2>
 
-        <div
-          className={`${styles.practiceContainer} ${
-            chatVisible ? styles.chatVisible : styles.chatHidden
-          }`}
-          onClick={hideMenu}
-        >
-          <div className={styles.taskContainer}>
-            <div
-              className={styles.task}
-              onContextMenu={showMenu}
-            >
-              {question?.question_prompt}
-            </div>
+        {isDataLoading ? (
+        <Spin size="large" />
+        ) : (
+          <div
+            className={`${styles.practiceContainer} ${
+              chatVisible ? styles.chatVisible : styles.chatHidden
+            }`}
+            onClick={hideMenu}
+          >
+            <div className={styles.taskContainer}>
+              <div
+                className={styles.task}
+                onContextMenu={showMenu}
+              >
+                {question?.question_prompt}
+              </div>
 
-            {part !== '2' && (
-              <div className={styles.imgContainer}>
-                <img
-                  src={question?.question_image_urls[0]}
-                  alt="IELTS Writing Task 1"
+              {part !== '2' && (
+                <div className={styles.imgContainer}>
+                  <img
+                    src={question?.question_image_urls[0]}
+                    alt="IELTS Writing Task 1"
+                  />
+                  <WritingFunctionMenu
+                    visible={menuVisible}
+                    position={menuPosition}
+                    onMenuItemClick={showChat}
+                  />
+                </div>
+              )}
+              <div className={styles.answerHeader}>
+                <div className={styles.instruction}>
+                  <img
+                    className={styles.instructionImg}
+                    src="/images/logo/main.svg"
+                    alt=""
+                  />
+                  <p className={styles.textPracticing}>
+                    {question?.question_prompt.split('.')[0]}
+                  </p>
+                </div>
+                <CountdownClock />
+              </div>
+              <div className={styles.answerContainer}>
+                <p className={styles.textPracticing}>Trả lời:</p>
+                <textarea
+                  ref={textareaRef}
+                  className={styles.answerInput}
+                  onContextMenu={showMenu}
                 />
                 <WritingFunctionMenu
                   visible={menuVisible}
@@ -288,65 +289,40 @@ export default function PracticePage() {
                   onMenuItemClick={showChat}
                 />
               </div>
-            )}
-            <div className={styles.answerHeader}>
-              <div className={styles.instruction}>
-                <img
-                  className={styles.instructionImg}
-                  src="/images/logo/main.svg"
-                  alt=""
-                />
-                <p className={styles.textPracticing}>
-                  You should spend about 20 minutes on this task
-                </p>
-              </div>
-              <CountdownClock />
-            </div>
-            <div className={styles.answerContainer}>
-              <p className={styles.textPracticing}>Trả lời:</p>
-              <textarea
-                ref={textareaRef}
-                className={styles.answerInput}
-                onContextMenu={showMenu}
-              />
-              <WritingFunctionMenu
-                visible={menuVisible}
-                position={menuPosition}
-                onMenuItemClick={showChat}
-              />
-            </div>
-            <div className={styles.practiceButtonContainer}>
-              <Link href="/ielts/writing">
-                <button className={styles.redButton}>Thoát</button>
-              </Link>
+              <div className={styles.practiceButtonContainer}>
+                <Link href="/ielts">
+                  <button className={styles.redButton}>Thoát</button>
+                </Link>
 
-              <Link
-                href={
-                  part === 'all'
-                    ? '/ielts/writing/practicing?part=2'
-                    : {
-                        pathname: '/ielts/writing/result',
-                      }
-                }
-              >
-                <button
-                  className={styles.primaryButton}
-                  onClick={handleDoneButtonClick}
+                <Link
+                  href={
+                    part === 'all'
+                      ? `/ielts/writing/${params.id}/2`
+                      : {
+                          pathname: '/ielts/writing/result',
+                        }
+                  }
                 >
-                  Xong
-                </button>
-              </Link>
+                  <button
+                    className={styles.primaryButton}
+                    onClick={handleDoneButtonClick}
+                  >
+                    Xong
+                  </button>
+                </Link>
+              </div>
             </div>
-          </div>
 
-          {chatVisible && (
-            <div>
-              <CloseChatContext.Provider value={hideChat}>
-                <BuddieSupport requests={chatRequests} setRequests={setChatRequests} isProcessing={isChatProcessing} setIsProcessing={setIsChatProcessing}/>
-              </CloseChatContext.Provider>
-            </div>
-          )}
-        </div>
+            {chatVisible && (
+              <div>
+                <CloseChatContext.Provider value={hideChat}>
+                  <BuddieSupport requests={chatRequests} setRequests={setChatRequests} isProcessing={isChatProcessing} setIsProcessing={setIsChatProcessing}/>
+                </CloseChatContext.Provider>
+              </div>
+            )}
+          </div>
+        )}
+
       </div>
     </>
   );
