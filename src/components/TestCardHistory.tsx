6e074367c777003175d2163e37deccbc6501d848
thanhtrietla <20120222@student.hcmus.edu.vn>
2024-06-02 21:01:02 +0700

    Add Test History Outline

---
+++ src/components/TestCardHistory.tsx
@@ -0,0 +1,162 @@
+'use client';
+import { ClockCircleOutlined } from '@ant-design/icons';
+import styles from '@/styles/components/TestCardHistory.module.scss';
+import { Button, Modal, Select } from 'antd';
+import { SetStateAction, useState } from 'react';
+
+import Link from 'next/link';
+interface user {
+  user_id: string;
+  display_name: string;
+  photo_url: string;
+}
+export default function TestCardHistory(props: {
+  setPageLoading: React.Dispatch<SetStateAction<boolean>>;
+  submissionCount?: number;
+  review?: { star: number; count: number };
+  user?: user;
+  isUserTest?: boolean;
+  testName: string;
+  testDuration?: string;
+  testTags?: string[];
+  testSkill?: string;
+  testId: string;
+  partIds: string[];
+}) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+  const handleClick = () => {
+    showModal();
+  };
+  const handleSelectChange = (value: string) => {
+    console.log(value);
+  };
+
+  const skill = props.testSkill?.split('_')[1];
+  const duration = Number(props.testDuration) / 60;
+
+  const partSelectButtons = (
+    <div className={styles.partSelect}>
+      {props.partIds.map((part, index) => {
+        return (
+          <Link
+            href={`ielts/${skill}/${props.testId}/${index + 1}`}
+            key={part}
+          >
+            <Button
+              key={part}
+              type="primary"
+              onClick={() => props.setPageLoading(true)}
+            >
+              Part {index + 1}
+            </Button>
+          </Link>
+        );
+      })}
+    </div>
+  );
+  return (
+    <div className={styles.cardWrapper}>
+      <h3 className={styles.header}>{props.testName}</h3>
+      {!props.isUserTest && (
+        <>
+          <div className={styles.clock}>
+            <ClockCircleOutlined />
+            {duration} phút
+          </div>
+          <div className={styles.testTagWrapper}>
+            {props.testTags?.map((tag) => {
+              return (
+                <span
+                  className={styles.testTag}
+                  key={tag}
+                >
+                  {tag}
+                </span>
+              );
+            })}
+          </div>
+          <Button
+            onClick={handleClick}
+            className={styles.button}
+          >
+            Làm lại
+          </Button>
+        </>
+      )}
+      {props.isUserTest && (
+        <>
+            <div className={styles.score}>
+                85%
+            </div>
+            <div>
+                48/60 phút
+            </div>
+            <Link href={`/tests/${props.testId}`}>
+                <Button
+                className={styles.button}
+                onClick={() => props.setPageLoading(true)}
+                >
+                Làm lại
+                </Button>
+            </Link>
+        </>
+      )}
+      <Modal
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        footer={null}
+      >
+        <div className={styles.modal}>
+          <h3>{props.testName}</h3>
+          <div className={styles.testTagWrapperModal}>
+            {props.testTags?.map((tag) => {
+              return (
+                <span
+                  className={styles.testTag}
+                  key={tag}
+                >
+                  {tag}
+                </span>
+              );
+            })}
+          </div>
+          <div className={styles.modalSelect}>
+            Chọn thời gian
+            <Select
+              defaultValue="không giới hạn"
+              options={[
+                { value: 'unlimied', label: 'không giới hạn' },
+                { value: 'dependent', label: 'theo thời gian của đề' },
+              ]}
+              style={{ width: '200px' }}
+              onChange={handleSelectChange}
+            />
+          </div>
+          {partSelectButtons}
+          <Link href={`ielts/${skill}/${props.testId}/all`}>
+            <Button
+              type="primary"
+              className={styles.purpleBtn}
+              onClick={() => props.setPageLoading(true)}
+            >
+              Luyện tập tất cả
+            </Button>
+          </Link>
+        </div>
+      </Modal>
+    </div>
+  );
+}
