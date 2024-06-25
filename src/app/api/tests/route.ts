73b93466cb34daceba0ffe751ed1eb6809e6404c
thanhtrietla <20120222@student.hcmus.edu.vn>
2024-06-26 01:00:04 +0700

    Complete Create Test

--- src/app/api/tests/route.ts
+++ src/app/api/tests/route.ts
@@ -1,4 +1,6 @@
+import { uploadBuffer } from '@/lib';
 import { NextRequest, NextResponse } from 'next/server';
+import { v4 as uuid } from 'uuid';
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
@@ -7,6 +9,7 @@ export async function GET(request: NextRequest) {
   const search = searchParams.get('search');
   const isbuddie = searchParams.get('isbuddie');
   //const queryString = `${process.env.API_BASE_URL}/api/v1/ai/synonyms?word=speed`;
+  const offset = page ? Number(page) - 1 : 0;
   const offset = page ? Number(page) - 1 : 0;
   let queryString = `${process.env.API_BASE_URL}/api/v1/tests?limit=10&offset=${offset}&access=public&is_buddie_test=${isbuddie}`;
   if (test_type) {
@@ -25,7 +28,193 @@ export async function GET(request: NextRequest) {
     const data = await response.json();
     return NextResponse.json(data);
   } catch (error) {
-    console.error(error);
     return NextResponse.json({ error });
   }
 }
+
+export const POST = async function createTest(req: Request) {
+  try {
+    const reqData = await req.formData();
+    const test = JSON.parse(reqData.get('test') as string);
+    const testRecordingFile = reqData.get('test_recording') as File;
+    const partRecordingFiles = reqData.getAll('part_recording') as File[];
+    const partImages = reqData.getAll('part_images') as File[];
+    const questionGroupRecordingFiles = reqData.getAll(
+      'question_group_recording'
+    ) as File[];
+    const questionGroupImages = reqData.getAll(
+      'question_group_images'
+    ) as File[];
+    const questionRecordingFiles = reqData.getAll(
+      'question_recording'
+    ) as File[];
+    const questionImages = reqData.getAll('question_images') as File[];
+
+    if (testRecordingFile) {
+      const downloadUrl = await uploadBuffer(
+        Buffer.from(await testRecordingFile.arrayBuffer()),
+        `tests/${uuid()}/${testRecordingFile.name}`
+      );
+      test.test_recording = downloadUrl;
+    }
+
+    const uploadTasks: Promise<void>[] = [];
+
+    if (partRecordingFiles.length > 0) {
+      partRecordingFiles.forEach((partRecordingFile) => {
+        if (partRecordingFile) {
+          uploadTasks.push(
+            (async () => {
+              const downloadUrl = await uploadBuffer(
+                Buffer.from(await partRecordingFile.arrayBuffer()),
+                `tests/${uuid()}/${partRecordingFile.name}`
+              );
+              const partIndex =
+                parseInt(partRecordingFile.name.split('-')[1].split('.')[0]) -
+                1;
+              test.parts[partIndex].part_recording = downloadUrl;
+            })()
+          );
+        }
+      });
+    }
+
+    if (partImages.length > 0) {
+      partImages.forEach((partImage) => {
+        if (partImage) {
+          uploadTasks.push(
+            (async () => {
+              const downloadUrl = await uploadBuffer(
+                Buffer.from(await partImage.arrayBuffer()),
+                `tests/${uuid()}/${partImage.name}`
+              );
+              const partIndex = parseInt(partImage.name.split('-')[1]) - 1;
+
+              test.parts[partIndex].part_image_urls.push(downloadUrl);
+            })()
+          );
+        }
+      });
+    }
+
+    if (questionGroupRecordingFiles.length > 0) {
+      questionGroupRecordingFiles.forEach((questionGroupRecordingFile) => {
+        if (questionGroupRecordingFile) {
+          uploadTasks.push(
+            (async () => {
+              const downloadUrl = await uploadBuffer(
+                Buffer.from(await questionGroupRecordingFile.arrayBuffer()),
+                `tests/${uuid()}/${questionGroupRecordingFile.name}`
+              );
+
+              const partIndex =
+                parseInt(questionGroupRecordingFile.name.split('-')[1]) - 1;
+              const questionGroupIndex =
+                parseInt(
+                  questionGroupRecordingFile.name.split('-')[2].split('.')[0]
+                ) - 1;
+
+              test.parts[partIndex].question_groups[
+                questionGroupIndex
+              ].question_groups_info.question_group_recording = downloadUrl;
+            })()
+          );
+        }
+      });
+    }
+
+    if (questionGroupImages.length > 0) {
+      questionGroupImages.forEach((questionGroupImage) => {
+        if (questionGroupImage) {
+          uploadTasks.push(
+            (async () => {
+              const downloadUrl = await uploadBuffer(
+                Buffer.from(await questionGroupImage.arrayBuffer()),
+                `tests/${uuid()}/${questionGroupImage.name}`
+              );
+              const partIndex =
+                parseInt(questionGroupImage.name.split('-')[1]) - 1;
+              const questionGroupIndex =
+                parseInt(questionGroupImage.name.split('-')[2]) - 1;
+
+              test.parts[partIndex].question_groups[
+                questionGroupIndex
+              ].question_groups_info.question_group_image_urls.push(
+                downloadUrl
+              );
+            })()
+          );
+        }
+      });
+    }
+
+    if (questionGroupRecordingFiles.length > 0) {
+      questionRecordingFiles.forEach((questionRecordingFile) => {
+        if (questionRecordingFile) {
+          uploadTasks.push(
+            (async () => {
+              const downloadUrl = await uploadBuffer(
+                Buffer.from(await questionRecordingFile.arrayBuffer()),
+                `tests/${uuid()}/${questionRecordingFile.name}`
+              );
+
+              const partIndex =
+                parseInt(questionRecordingFile.name.split('-')[1]) - 1;
+              const questionGroupIndex =
+                parseInt(questionRecordingFile.name.split('-')[2]) - 1;
+              const questionIndex =
+                parseInt(
+                  questionRecordingFile.name.split('-')[3].split('.')[0]
+                ) - 1;
+
+              test.parts[partIndex].question_groups[
+                questionGroupIndex
+              ].questions[questionIndex].question_recording = downloadUrl;
+            })()
+          );
+        }
+      });
+    }
+
+    if (questionImages.length > 0) {
+      questionImages.forEach((questionImage) => {
+        if (questionImage) {
+          uploadTasks.push(
+            (async () => {
+              const downloadUrl = await uploadBuffer(
+                Buffer.from(await questionImage.arrayBuffer()),
+                `tests/${uuid()}/${questionImage.name}`
+              );
+
+              const partIndex = parseInt(questionImage.name.split('-')[1]) - 1;
+              const questionGroupIndex =
+                parseInt(questionImage.name.split('-')[2]) - 1;
+              const questionIndex =
+                parseInt(questionImage.name.split('-')[3]) - 1;
+
+              test.parts[partIndex].question_groups[
+                questionGroupIndex
+              ].questions[questionIndex].question_image_urls.push(downloadUrl);
+            })()
+          );
+        }
+      });
+    }
+
+    await Promise.all(uploadTasks);
+
+    const response = await fetch(`${process.env.API_BASE_URL}/api/v1/tests`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: req.headers.get('Authorization') || '',
+      },
+      body: JSON.stringify(test),
+    });
+    const data = await response.json();
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error });
+  }
+};
