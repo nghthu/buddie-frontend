19b6d973fc370359c28ef8b0b2891a445288c077
thanhtrietla <20120222@student.hcmus.edu.vn>
2024-06-29 19:16:03 +0700

    Revert change

--- src/app/(user)/ielts/writing/[id]/[part]/page.tsx
+++ src/app/(user)/ielts/writing/[id]/[part]/page.tsx
@@ -390,7 +390,7 @@ export default function PracticePage({
                 className={styles.task}
                 onContextMenu={showMenu}
               >
-                {question?.question_prompt.split(/\.:|:/).slice(1).join('.')}
+                {question?.question_prompt.split('.').slice(1).join('.')}
               </div>
 
               {part !== '2' && (
@@ -414,7 +414,7 @@ export default function PracticePage({
                     alt=""
                   />
                   <p className={styles.textPracticing}>
-                    {question?.question_prompt.split(/\.:|:/)[0]}
+                    {question?.question_prompt.split('.')[0]}
                   </p>
                 </div>
                 {/* <CountdownClock /> */}
