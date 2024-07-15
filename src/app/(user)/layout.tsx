c0a5c28bc8874165a3975844d0c6e72dc14b4ce7
Thuy Nguyen <thuy.nguyen@zoi.tech>
2024-07-16 02:38:16 +0700

    hotfix: update token when new verified user access to main page

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -6,13 +6,23 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '@/lib';
 import { useEffect } from 'react';
 import Header from '@/components/Header';
+import { getIdToken, User } from 'firebase/auth';
 
 const UserLayout = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
-  const [user, loading, error] = useAuthState(auth);
+  const onUserChanged = async (currentUser: User | null): Promise<void> => {
+    if (currentUser) {
+      await getIdToken(currentUser, true);
+    }
+  };
+
+  const [user, loading, error] = useAuthState(auth, {
+    onUserChanged,
+  });
+
   const router = useRouter();
 
   useEffect(() => {
