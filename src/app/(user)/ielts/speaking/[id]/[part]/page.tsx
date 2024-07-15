5cb316183ad8851f99a89fcc65c293d152188ca0
nghthu <hanhh.thu@gmail.com>
2024-07-16 01:14:31 +0700

    fix speaking

--- src/app/(user)/ielts/speaking/[id]/[part]/page.tsx
+++ src/app/(user)/ielts/speaking/[id]/[part]/page.tsx
@@ -245,54 +245,47 @@ const PracticeSpeaking = ({
   };
 
   const nextHandler = async () => {
-    if (currentQuestion === 0 && currentPart === 0) {
-      console.log('logcheck44');
+    if (currentQuestion === 0) {
       setCurrentQuestion(1);
       setInstruction(false);
     } else {
-      if (currentPart !== 0 && currentQuestion === 0) {
-        setCurrentPart((prevPart) => prevPart + 1);
-        setCurrentQuestion(0);
-      } else {
-        console.log('logcheck5');
-        if (testData) {
-          if (currentAnswer && wantToSubmit) {
-            console.log('kkk', testData), currentQuestion;
-            setSubmitAnswers((prevAnswers) => [
-              ...prevAnswers,
-              {
-                _id: testData.questions[currentQuestion]._id,
-                answer_result: { user_answer: '' },
-                audio: currentAnswer,
-                transcript: transcript,
-              },
-            ]);
-            console.log('logcheck2', submitAnswers, submitAnswers);
-          }
-          if (currentQuestion < testData.questions.length - 1) {
-            console.log('logcheck', currentQuestion);
-            setCurrentQuestion((prevQuestion) => prevQuestion + 1);
-          } else if (currentQuestion === testData.questions.length - 1) {
-            // handle submit
-            console.log('submit');
-            const token = await user?.getIdToken();
-            const formData = new FormData();
+      if (testData) {
+        if (currentAnswer && wantToSubmit) {
+          console.log('kkk', testData), currentQuestion;
+          setSubmitAnswers((prevAnswers) => [
+            ...prevAnswers,
+            {
+              _id: testData.questions[currentQuestion]._id,
+              answer_result: { user_answer: '' },
+              audio: currentAnswer,
+              transcript: transcript,
+            },
+          ]);
+          console.log('logcheck2', submitAnswers, submitAnswers);
+        }
+        if (currentQuestion < testData.questions.length - 1) {
+          console.log('logcheck', currentQuestion);
+          setCurrentQuestion((prevQuestion) => prevQuestion + 1);
+        } else if (currentQuestion === testData.questions.length - 1) {
+          // handle submit
+          console.log('submit');
+          const token = await user?.getIdToken();
+          const formData = new FormData();
 
-            submitAnswers.forEach((answer) => {
-              formData.append(`audios`, answer.audio, `{${answer._id}.webm`);
-            });
-            console.log(currentPart);
-            formData.append('part', (currentPart + 1).toString());
-            formData.append('testId', params.id);
+          submitAnswers.forEach((answer) => {
+            formData.append(`audios`, answer.audio, `{${answer._id}.webm`);
+          });
+          console.log(currentPart);
+          formData.append('part', (currentPart + 1).toString());
+          formData.append('testId', params.id);
 
-            await fetch(`/api/file`, {
-              method: 'POST',
-              headers: {
-                authorization: `Bearer ${token}`,
-              },
-              body: formData,
-            });
-          }
+          await fetch(`/api/file`, {
+            method: 'POST',
+            headers: {
+              authorization: `Bearer ${token}`,
+            },
+            body: formData,
+          });
         }
       }
     }
@@ -305,81 +298,80 @@ const PracticeSpeaking = ({
     const nextPart = currentPart + 1;
     setCurrentPart((prevPart) => prevPart + 1);
     setTestData(data.parts[nextPart].question_groups[0]);
-    console.log('logcheck3', data.parts[nextPart].question_groups[0]);
-    setCurrentQuestion(0);
+    setCurrentQuestion(1);
     // handle submit
-    // console.log('submit');
-    // const token = await user?.getIdToken();
-    // const formData = new FormData();
+    console.log('submit');
+    const token = await user?.getIdToken();
+    const formData = new FormData();
 
-    // submitAnswers.forEach((answer) => {
-    //   formData.append(`audios`, answer.audio, `{${answer._id}.webm`);
-    // });
-    // console.log(currentPart);
-    // formData.append('part', (currentPart + 1).toString());
-    // formData.append('testId', params.id);
+    submitAnswers.forEach((answer) => {
+      formData.append(`audios`, answer.audio, `{${answer._id}.webm`);
+    });
+    console.log(currentPart);
+    formData.append('part', (currentPart + 1).toString());
+    formData.append('testId', params.id);
 
-    // const response = await fetch(`/api/file`, {
-    //   method: 'POST',
-    //   headers: {
-    //     authorization: `Bearer ${token}`,
-    //   },
-    //   body: formData,
-    // });
+    const response = await fetch(`/api/file`, {
+      method: 'POST',
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+      body: formData,
+    });
 
-    // const responseData: AudioResponse[] =
-    //   (await response.json()) as AudioResponse[];
+    const responseData: AudioResponse[] =
+      (await response.json()) as AudioResponse[];
 
-    // submitAnswers.forEach((answer) => {
-    //   const answerId = answer._id;
+    submitAnswers.forEach((answer) => {
+      const answerId = answer._id;
 
-    //   responseData.forEach((item) => {
-    //     const filename = item.filename;
-    //     if (filename.includes(answerId)) {
-    //       // Perform your logic here if filename matches
-    //       console.log(
-    //         `Match found for answer ID ${answerId} with filename ${filename}`
-    //       );
-    //       // Example logic: Update UI or perform additional actions
-    //     }
-    //   });
-    // });
+      responseData.forEach((item) => {
+        const filename = item.filename;
+        if (filename.includes(answerId)) {
+          // Perform your logic here if filename matches
+          console.log(
+            `Match found for answer ID ${answerId} with filename ${filename}`
+          );
+          // Example logic: Update UI or perform additional actions
+        }
+      });
+    });
 
-    // const structuredAnswers = {
-    //   test_id: data._id,
-    //   parts: data.parts.map((part: Part, index: number) => ({
-    //     part_number: index + 1,
-    //     _id: part._id,
-    //     question_groups: part.question_groups.map(
-    //       (questionGroup: QuestionGroup) => ({
-    //         _id: questionGroup._id,
-    //         questions: questionGroup.questions.map((question) => {
-    //           const userAnswer = submitAnswers.find(
-    //             (answer) => answer._id === question._id
-    //           );
-    //           let audioKey = '';
-    //           if (userAnswer) {
-    //             responseData.forEach((item) => {
-    //               const filename = item.filename;
-    //               if (filename.includes(userAnswer._id)) {
-    //                 audioKey = item.key;
-    //               }
-    //             });
-    //           }
+    const structuredAnswers = {
+      test_id: data._id,
+      parts: data.parts.map((part: Part, index: number) => ({
+        part_number: index + 1,
+        _id: part._id,
+        question_groups: part.question_groups.map(
+          (questionGroup: QuestionGroup) => ({
+            _id: questionGroup._id,
+            questions: questionGroup.questions.map((question) => {
+              const userAnswer = submitAnswers.find(
+                (answer) => answer._id === question._id
+              );
+              let audioKey = '';
+              if (userAnswer) {
+                responseData.forEach((item) => {
+                  const filename = item.filename;
+                  if (filename.includes(userAnswer._id)) {
+                    audioKey = item.key;
+                  }
+                });
+              }
 
-    //           return {
-    //             _id: question._id,
-    //             answer_result: {
-    //               user_answer: audioKey ? audioKey : '',
-    //             },
-    //           };
-    //         }),
-    //       })
-    //     ),
-    //   })),
-    // };
+              return {
+                _id: question._id,
+                answer_result: {
+                  user_answer: audioKey ? audioKey : '',
+                },
+              };
+            }),
+          })
+        ),
+      })),
+    };
 
-    // console.log('ddd', structuredAnswers);
+    console.log('ddd', structuredAnswers);
   };
 
   const setSubmitHandler: CheckboxProps['onChange'] = (e) => {
@@ -442,24 +434,24 @@ const PracticeSpeaking = ({
 
       console.log('logcheck2', structuredAnswers);
 
-      // const submissionResponse: Response = await fetch(
-      //   `/api/test-submissions`,
-      //   {
-      //     method: 'POST',
-      //     headers: {
-      //       authorization: `Bearer ${token}`,
-      //       'Content-Type': 'application/json',
-      //     },
-      //     body: JSON.stringify(structuredAnswers),
-      //   }
-      // );
+      const submissionResponse: Response = await fetch(
+        `/api/test-submissions`,
+        {
+          method: 'POST',
+          headers: {
+            authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(structuredAnswers),
+        }
+      );
 
-      // const submissionData = await submissionResponse.json();
-      // router.push(
-      //   `/result?speaking=true&testId=${params.id}&testSubmissionId=${submissionData._id}&part=${params.part}`
-      // );
+      const submissionData = await submissionResponse.json();
+      router.push(
+        `/result?speaking=true&testId=${params.id}&testSubmissionId=${submissionData._id}&part=${params.part}`
+      );
 
-      // console.log('Send Answer: ', structuredAnswers);
+      console.log('Send Answer: ', structuredAnswers);
     }
   };
 
