2b0022a3dbeac05f8c6a5fc9c1036aa121edd2ff
nghthu <hanhh.thu@gmail.com>
2024-07-22 23:33:25 +0700

    fix

--- src/app/(user)/tests/page.tsx
+++ src/app/(user)/tests/page.tsx
@@ -2,39 +2,17 @@
 
 import SkillHeader from '@/components/SkillHeader';
 import TestLibrary from '@/components/TestLibrary';
-import { useState, useEffect } from 'react';
-import { auth } from '@/lib';
-import AdminDashboard from '@/components/AdminDashboard';
+import { useState } from 'react';
 
 const UserTestLibrary = () => {
   const [pageLoading, setPageLoading] = useState(false);
-  const [isAdmin, setIsAdmin] = useState(false);
-
-  console.log('logcheck1', isAdmin);
-
-  useEffect(() => {
-    async function fetchAdminStatus() {
-      const tokenResult = await auth.currentUser?.getIdTokenResult();
-      const isAdmin = tokenResult?.claims.admin;
-      setIsAdmin(!!isAdmin);
-    }
-
-    fetchAdminStatus();
-  }, []);
-
   return (
     <>
-      {!isAdmin && (
-        <div>
-          <SkillHeader title={'Đề thi IELTS'}></SkillHeader>
-          <TestLibrary
-            pageLoading={pageLoading}
-            setPageLoading={setPageLoading}
-          />
-        </div>
-      )}
-
-      {isAdmin && <AdminDashboard />}
+      <SkillHeader title={'Đề thi IELTS'}></SkillHeader>
+      <TestLibrary
+        pageLoading={pageLoading}
+        setPageLoading={setPageLoading}
+      />
     </>
   );
 };
